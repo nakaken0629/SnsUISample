e07f4c0b3d768f18dd9b88424eed5b856795e98d
NAKAGAKI Kenji <nakaken0629@gmail.com>
2016-03-15 23:57:30 +0900

    ベースとなるViewControllerを作成

---
+++ SnsUISample/ListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ListViewController.swift
+//  SnsUISample
+//
+//  Created by 中垣 健志 on 2016/03/15.
+//  Copyright © 2016年 itvirtuoso. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController : UIViewController {
+    
+}
